55f7e1f8c33ae7881bd142ba9346367d510cc8e9
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-06-28 10:03:13 -0400

    Changed colorf and made some updates

--- interactive-form-v1/js/script.js
+++ interactive-form-v1/js/script.js
@@ -104,7 +104,7 @@ addActivityListener('npm', 100);
 
 // EFFECTS: adds a change listener to an activity checkbox,
 //          when checked, calculates the price of the total and updates the html text
-function addActivityListener(NameofActivity, price, conflictName = '') {
+function addActivityListener(NameofActivity, price, NameOfConflict = '') {
 
 	const $activity = $('.activities input[name=' + NameofActivity + ']');
 
@@ -113,15 +113,15 @@ function addActivityListener(NameofActivity, price, conflictName = '') {
 			total += price;
 			$('.total p').text('Total: $' + total);
 			// in case of conflict, toggle the conflicting activity
-			if (conflictName !== '') {
-				toggleActivity(conflictName, false);
+			if (NameOfConflict !== '') {
+				toggleActivity(NameOfConflict, false);
 			}
 		}
 		else {
 			total -= price;
 			$('.total p').text('Total: $' + total);
-			if (conflictName !== '') {
-				toggleActivity(conflictName, true);
+			if (NameOfConflict !== '') {
+				toggleActivity(NameofConflict, true);
 			}
 		}
 	});
@@ -130,7 +130,7 @@ function addActivityListener(NameofActivity, price, conflictName = '') {
 // CHANGES: toggles the activity to be enabled or disabled based on "enable" boolean
 function toggleActivity(NameofActivity, enable) {
 
-	const $activity = $('.activities input[name=' + activityName + ']');
+	const $activity = $('.activities input[name=' + NameOfActivity + ']');
 
 	if (enable) {
 		$activity.prop('disabled', false);
@@ -193,3 +193,143 @@ $payment.change(() => {
 	}
 });
 
+// ************************************ //
+// FORM VALIDATION FUNCTIONS AND EVENTS //
+// ************************************ //
+
+// EFFECTS: Compares the regEx to the input val and returns true if matched
+//          Adds an error class and appends an error message when not matched
+function isValidInput($input, regEx, val, message = '') {
+
+	if (regEx.test(val)) {
+		// Remove possible error class and error message when test passes
+		$input.removeClass('error');
+
+		if ($input.prev().filter('.error-message').length !== 0) {
+			$input.prev().remove();
+		}
+
+		return true;
+	}
+	else {
+		$input.addClass('error');
+
+		// Check for empty versus malformatted input, display appropriate message
+		if (val === '') {
+			message = ('Please enter your ').concat(message);
+		}
+		else {
+			message = 'Your ' + message + ' is formatted incorrectly'
+		}
+
+		// Check for existence of error message, add html to document, or change text
+		if ($input.prev().filter('.error-message').length === 0)
+			$input.before('<p class="error-message">' + message + '</p>');
+		else {
+			$input.prev().filter('.error-message').text(message);
+		}
+
+		return false;
+	}
+
+}
+
+// EFFECTS: Checks for at least one selected (checked) activity, returns false otherwise
+//          Handles error message adding/removing depending on check
+function isAttendingActivity() {
+
+	const $activities = $('.activities input');
+	const $errorHTML = $('<p class="error-message">Please select at least one activity.</p>');
+
+	// If there is at least 1 checked activity, returns true
+	if ($activities.filter(':checked').length) {
+		$('.activities > .error-message').remove();
+		return true;
+	}
+	else {
+		if ($('.activities > .error-message').length === 0)
+			$('.activities legend').after($errorHTML);
+		return false;
+	}
+}
+
+// EFFECTS: Call this function in order to add real-time form validation
+//          Currently only supports Name and Email input fields
+function realtimeValidation() {
+
+	const nameRegEx = /[a-zA-Z]{1,}/;
+
+	const $emailInput = $('#mail');
+	const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+	// These listeners bind to input events, and check the validity of each input
+	$nameInput.bind('input', () => { isValidInput($nameInput, nameRegEx, $nameInput.val(), 'name') });
+	$emailInput.bind('input', () => { isValidInput($emailInput, emailRegEx, $emailInput.val(), 'email') });
+}
+
+
+// EFFECTS: Calls input validation functions and returns true if all pass
+//          If one or more validation functions fail, the function returns false
+function validateForm() {
+
+	const nameRegEx = /[a-zA-Z]{1,}/;
+	const nameVal = $nameInput.val();
+
+	const $emailInput = $('#mail');
+	const emailRegEx = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	const emailVal = $emailInput.val();
+
+	const $ccInput = $('#cc-num');
+	const ccRegEx = /^[0-9]{13,16}$/;
+	const ccVal = $ccInput.val();
+
+	const $zipInput = $('#zip');
+	const zipRegEx = /^[0-9]{5}$/;
+	const zipVal = $zipInput.val();
+
+	const $cvvInput = $('#cvv');
+	const cvvRegEx = /^[0-9]{3}$/;
+	const cvvVal = $cvvInput.val();
+
+	let isValid = true;
+
+	// Each condition calls valid check. If the check returns false, we flip the isValid flag
+	if (!isValidInput($nameInput, nameRegEx, nameVal, 'name')) {
+		isValid = false;
+	}
+	if (!isValidInput($emailInput, emailRegEx, emailVal, 'email')) {
+		isValid = false;
+	}
+	if (!isAttendingActivity()) {
+		isValid = false;
+	}
+
+	// Only check for credit card validation if "Credit Card" payment type is selected
+	if ($('#payment option').filter(':selected').val() === 'credit card') {
+		if (!isValidInput($ccInput, ccRegEx, ccVal, 'card number')) {
+			isValid = false;
+		}
+		if (!isValidInput($zipInput, zipRegEx, zipVal, 'zip code')) {
+			isValid = false;
+		}
+		if (!isValidInput($cvvInput, cvvRegEx, cvvVal, 'CVV')) {
+			isValid = false;
+		}
+	}
+
+	// Return the flag, will be false if any of the tests failed
+	return isValid;
+}
+
+// Call the function for enabling real-time validation on the form
+realtimeValidation();
+
+// Listener on form submission, validates form fields, otherwise, prevents submission
+$('form').submit((ev) => {
+	if (validateForm()) {
+		return;
+	}
+	else {
+		ev.preventDefault();
+	}
+});
